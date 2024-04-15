5f2a8b22eb8baf2f97586301158a152a4988d402
emonhosen0130 <irfanmuhammad013090@gmail.com>
2024-04-15 08:13:29 +0600

    register and login from add

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,17 @@
+
+const Register = () => {
+    return (
+        <div className="mx-auto">
+            <h2 className="text-3xl">please Register</h2>
+            <form>
+                <input className="mb-4" type="email" name="email" id="" />
+                <br />
+                <input type="password" name="password" id="" />
+                <br />
+                <input type="submit" value="Register" />
+            </form>
+        </div>
+    ); 
+};
+
+export default Register;
